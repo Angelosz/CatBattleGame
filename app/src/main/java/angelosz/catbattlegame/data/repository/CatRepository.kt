39f322a559ce674e7f07b6ad5342a85452b84a63
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-11-26 14:36:31 +0000

    Changes CatRepository to Interface and Adds OfflineCatDaoRepository

---
+++ app/src/main/java/angelosz/catbattlegame/data/repository/CatRepository.kt
@@ -0,0 +1,11 @@
+package angelosz.catbattlegame.data.repository
+
+import angelosz.catbattlegame.domain.models.Cat
+
+interface CatRepository {
+    suspend fun getAllCats(): List<Cat>
+
+    suspend fun getCatById(id: Int): Cat
+
+    suspend fun InsertCats(cats: List<Cat>)
+}
---
+++ app/src/main/java/angelosz/catbattlegame/data/repository/OfflineCatDaoRepository.kt
@@ -0,0 +1,13 @@
+package angelosz.catbattlegame.data.repository
+
+import angelosz.catbattlegame.data.dao.CatDao
+import angelosz.catbattlegame.domain.models.Cat
+
+class OfflineCatDaoRepository(private val catDao: CatDao): CatRepository {
+
+    override suspend fun getAllCats() = catDao.getAllCats()
+
+    override suspend fun getCatById(id: Int) = catDao.getCatById(id)
+
+    override suspend fun InsertCats(cats: List<Cat>) = catDao.insertCats(cats)
+}
