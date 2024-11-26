1c9ab1132a0570c896199037ea1b8bdcc3d6ee67
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-11-26 14:22:09 +0000

    Creates Cat data class (Room Entity)

---
+++ app/src/main/java/angelosz/catbattlegame/domain/models/Cat.kt
@@ -0,0 +1,27 @@
+package angelosz.catbattlegame.domain.models
+
+import androidx.annotation.DrawableRes
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import angelosz.catbattlegame.domain.enums.ArmorType
+import angelosz.catbattlegame.domain.enums.CatRarity
+import angelosz.catbattlegame.domain.enums.CatRole
+
+@Entity(tableName = "cats")
+data class Cat(
+    @PrimaryKey
+    val id: Int,
+    val name: String,
+    val armorType: ArmorType,
+    val role: CatRole,
+    val baseHealth: Int,
+    val baseAttack: Int,
+    val baseDefense: Int,
+    val attackSpeed: Float,
+    @DrawableRes val image: Int,
+    val rarity: CatRarity,
+    val evolutionLevel: Int = 50,
+    val nextEvolutionId: Int? = null,
+    val evolutionItemId: Int? = null,
+    val isElderOf: Int? = null
+)
