bd938f61fad3ad01a6f525ee806f52a5f68f4a0d
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-11-28 09:18:13 +0000

    Cleans up MainActivity

--- app/src/main/java/angelosz/catbattlegame/MainActivity.kt
+++ app/src/main/java/angelosz/catbattlegame/MainActivity.kt
@@ -6,11 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
-import androidx.lifecycle.lifecycleScope
-import angelosz.catbattlegame.data.preloadData
 import angelosz.catbattlegame.ui.screens.AppLayout
 import angelosz.catbattlegame.ui.theme.CatBattleGameTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
@@ -19,11 +16,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        lifecycleScope.launch {
-            preloadData(applicationContext)
-        }
-
         setContent {
             CatBattleGameTheme {
                 AppLayout(windowSize = calculateWindowSizeClass(this).widthSizeClass)
