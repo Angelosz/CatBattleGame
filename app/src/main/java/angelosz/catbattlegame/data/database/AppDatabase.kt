e3e0132fbdf5a2f1eeca5fb5bb0faaa2c2340603
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-11-28 06:20:50 +0000

    Adds Ability, CatAbilityCrossRef, AbilityArmorDamageMultiplier classes(as tables) to the database; Adds function to get the AbilityDao from the database

--- app/src/main/java/angelosz/catbattlegame/data/database/AppDatabase.kt
+++ app/src/main/java/angelosz/catbattlegame/data/database/AppDatabase.kt
@@ -4,12 +4,23 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import angelosz.catbattlegame.data.dao.AbilityDao
 import angelosz.catbattlegame.data.dao.CatDao
-import angelosz.catbattlegame.domain.models.Cat
+import angelosz.catbattlegame.domain.models.entities.Ability
+import angelosz.catbattlegame.domain.models.entities.AbilityArmorDamageMultiplier
+import angelosz.catbattlegame.domain.models.entities.Cat
+import angelosz.catbattlegame.domain.models.entities.CatAbilityCrossRef
 
-@Database(entities = [Cat::class], version = 2)
+@Database(
+    entities = [
+        Cat::class,
+        Ability::class,
+        CatAbilityCrossRef::class,
+        AbilityArmorDamageMultiplier::class
+   ], version = 9)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun catDao(): CatDao
+    abstract fun abilityDao(): AbilityDao
 
     companion object {
         @Volatile
