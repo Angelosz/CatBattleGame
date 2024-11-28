61467facb04e1bbc481534b78dc1b45fae088f99
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-11-28 06:18:55 +0000

    Creates HomeScreen Composable, Calls HomeScreen Composable from MainActivity

--- app/src/main/java/angelosz/catbattlegame/MainActivity.kt
+++ app/src/main/java/angelosz/catbattlegame/MainActivity.kt
@@ -4,19 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.lifecycle.lifecycleScope
+import angelosz.catbattlegame.data.preloadData
+import angelosz.catbattlegame.ui.screens.HomeScreen
 import angelosz.catbattlegame.ui.theme.CatBattleGameTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
 
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
+        lifecycleScope.launch {
+            preloadData(applicationContext)
+        }
 
         setContent {
             CatBattleGameTheme {
+                HomeScreen(windowSize = calculateWindowSizeClass(this).widthSizeClass)
             }
         }
     }
+
+
 }
