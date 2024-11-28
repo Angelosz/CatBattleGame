58aaeee0307e8fe6a47bd3056953c1f1d83e2ffc
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-11-28 06:17:22 +0000

    Creates AbilityArmorDamageMultiplier data class(Room entity)

---
+++ app/src/main/java/angelosz/catbattlegame/domain/models/entities/AbilityArmorDamageMultiplier.kt
@@ -0,0 +1,12 @@
+package angelosz.catbattlegame.domain.models.entities
+
+import androidx.room.Entity
+import angelosz.catbattlegame.domain.enums.AbilityType
+import angelosz.catbattlegame.domain.enums.ArmorType
+
+@Entity(primaryKeys = ["abilityType", "armorType"], tableName = "ability_armor_damage_multiplier")
+data class AbilityArmorDamageMultiplier(
+    val abilityType: AbilityType,
+    val armorType: ArmorType,
+    val multiplier: Float
+)
