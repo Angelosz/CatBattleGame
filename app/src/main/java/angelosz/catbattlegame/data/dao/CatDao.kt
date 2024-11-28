3c7a67c63e41740f81f03bba01120276eed2b260
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-11-28 06:22:26 +0000

    Modifies CatDao path to Cat data class

--- app/src/main/java/angelosz/catbattlegame/data/dao/CatDao.kt
+++ app/src/main/java/angelosz/catbattlegame/data/dao/CatDao.kt
@@ -4,7 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import angelosz.catbattlegame.domain.models.Cat
+import angelosz.catbattlegame.domain.models.entities.Cat
 
 @Dao
 interface CatDao {
