42f7657cb0a9dcd4a92392b1fed4ef17e2fb04ce
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-11-29 20:16:59 +0000

    Updates AppContainer so it has both CatRepository and AbilityRepository

--- app/src/main/java/angelosz/catbattlegame/data/AppContainer.kt
+++ app/src/main/java/angelosz/catbattlegame/data/AppContainer.kt
@@ -2,13 +2,18 @@ package angelosz.catbattlegame.data
 
 import android.content.Context
 import angelosz.catbattlegame.data.database.AppDatabase
+import angelosz.catbattlegame.data.repository.AbilityRepository
+import angelosz.catbattlegame.data.repository.CatRepository
+import angelosz.catbattlegame.data.repository.OfflineAbilityRepository
 import angelosz.catbattlegame.data.repository.OfflineCatDaoRepository
 
 interface AppContainer {
-    val repository: OfflineCatDaoRepository
+    val catRepository: CatRepository
+    val abilityRepository: AbilityRepository
 }
 
 class AppDataContainer(context: Context): AppContainer{
     private val database: AppDatabase = AppDatabase.getInstance(context)
-    override val repository: OfflineCatDaoRepository = OfflineCatDaoRepository(database.catDao())
+    override val catRepository: OfflineCatDaoRepository = OfflineCatDaoRepository(database.catDao())
+    override val abilityRepository: OfflineAbilityRepository = OfflineAbilityRepository(database.abilityDao())
 }
