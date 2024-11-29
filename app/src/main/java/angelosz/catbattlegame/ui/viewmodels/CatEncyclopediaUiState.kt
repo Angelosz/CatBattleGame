75aea8db005f233d9c129fe58b03b1bb010f621b
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-11-29 20:17:27 +0000

    Implements CatEncyclopediaViewModel and its CatEncyclopediaUiState

---
+++ app/src/main/java/angelosz/catbattlegame/ui/viewmodels/CatEncyclopediaUiState.kt
@@ -0,0 +1,14 @@
+package angelosz.catbattlegame.ui.viewmodels
+
+import angelosz.catbattlegame.domain.models.CatDetailsData
+import angelosz.catbattlegame.domain.models.entities.Ability
+import angelosz.catbattlegame.domain.models.entities.Cat
+
+data class CatEncyclopediaUiState(
+    val cats: List<Cat> = emptyList(),
+    val abilities: List<Ability> = emptyList(),
+    val selectedCatData: CatDetailsData? = null,
+    val selectedAbility: Ability? = null,
+    val onDetailsView: Boolean = false,
+    val onCatView: Boolean = true
+)
---
+++ app/src/main/java/angelosz/catbattlegame/ui/viewmodels/CatEncyclopediaViewModel.kt
@@ -0,0 +1,88 @@
+package angelosz.catbattlegame.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import angelosz.catbattlegame.data.repository.AbilityRepository
+import angelosz.catbattlegame.data.repository.CatRepository
+import angelosz.catbattlegame.domain.models.CatDetailsData
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class CatEncyclopediaViewModel(
+    val catRepository: CatRepository,
+    val abilityRepository: AbilityRepository
+): ViewModel() {
+    private val _uiState: MutableStateFlow<CatEncyclopediaUiState> = MutableStateFlow(
+        CatEncyclopediaUiState()
+    )
+    val uiState: StateFlow<CatEncyclopediaUiState> = _uiState
+
+    private val catList = _uiState.value.cats
+    private val abilityList = _uiState.value.abilities
+
+
+    init {
+        viewModelScope.launch {
+            fetchAllCatData()
+            fetchAllAbilityData()
+            if(catList.isNotEmpty()) {
+                fetchCatDetails(catList[0].id)
+            }
+            if(abilityList.isNotEmpty()){
+                fetchAbilityData(abilityList[0].id)
+            }
+        }
+    }
+
+    private suspend fun fetchAllCatData() {
+        _uiState.update {
+            it.copy(cats = catRepository.getAllCats())
+        }
+    }
+
+    private suspend fun fetchAllAbilityData() {
+        _uiState.update {
+            it.copy(abilities = abilityRepository.getAllAbilities())
+        }
+    }
+
+    private suspend fun fetchCatDetails(catId: Int) {
+        val cat = catRepository.getCatById(catId)
+        val details = CatDetailsData(
+            cat = cat,
+            abilities = abilityRepository.getCatAbilities(catId),
+            isElderOf = cat.isElderOf?.let { id -> catRepository.getCatById(id).name } ?: "",
+            nextEvolutionName = cat.nextEvolutionId?.let { id -> catRepository.getCatById(id).name }
+                ?: ""
+        )
+        _uiState.update{
+            it.copy(selectedCatData = details)
+        }
+    }
+
+    private suspend fun fetchAbilityData(abilityId: Int){
+        _uiState.update {
+            it.copy(selectedAbility = abilityRepository.getAbilityById(abilityId))
+        }
+    }
+
+    fun updateCardDetails(catId: Int){
+        viewModelScope.launch {
+            fetchCatDetails(catId)
+        }
+    }
+
+    fun selectAbility(abilityId: Int){
+        viewModelScope.launch {
+            fetchAbilityData(abilityId)
+        }
+    }
+
+    fun changeView(toDetailView: Boolean){
+        _uiState.update {
+            it.copy(onDetailsView = toDetailView)
+        }
+    }
+}
