eeff908000efdff1c494e70049467a2e2a0d24b8
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-11-29 20:20:07 +0000

    Updates AppLayout so we implement the Navigation system with HomeScreen and EncyclopediaScreen

--- app/src/main/java/angelosz/catbattlegame/ui/screens/AppLayout.kt
+++ app/src/main/java/angelosz/catbattlegame/ui/screens/AppLayout.kt
@@ -1,14 +1,39 @@
 package angelosz.catbattlegame.ui.screens
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 @Composable
-fun AppLayout(windowSize: WindowWidthSizeClass){
-    Scaffold (modifier = Modifier.fillMaxSize()) { innerPadding ->
-        HomeScreen(innerPadding)
+fun AppLayout(
+    windowSize: WindowWidthSizeClass
+){
+    val navController = rememberNavController()
+    Text(
+        text = "Home"
+    )
+    NavHost(
+        navController = navController,
+        startDestination = HomeScreenRoute,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        composable<HomeScreenRoute> {
+            HomeScreen(
+                windowSize = windowSize,
+                navigateToEncyclopedia = { navController.navigate(EncyclopediaScreenRoute) }
+            )
+        }
+        composable<EncyclopediaScreenRoute> {
+            EncyclopediaScreen(
+                windowSize = windowSize,
+                onBackPressed = { navController.navigateUp() }
+            )
+        }
     }
 }
