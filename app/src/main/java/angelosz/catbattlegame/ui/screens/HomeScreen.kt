c6687ebbddddac3d6ecdd5fc385c033237a5415b
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-11-30 23:43:25 +0000

    Updates Homescreen to use the new images as buttons

--- app/src/main/java/angelosz/catbattlegame/ui/screens/HomeScreen.kt
+++ app/src/main/java/angelosz/catbattlegame/ui/screens/HomeScreen.kt
@@ -1,21 +1,24 @@
 package angelosz.catbattlegame.ui.screens
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -28,14 +31,16 @@ fun HomeScreen(
     navigateToEncyclopedia: () -> Unit
 ) {
     if(windowSize == WindowWidthSizeClass.Expanded){
-        LandscapeHomeScreen(navigateToEncyclopedia)
+        LandscapeHomeScreen(navigateToEncyclopedia,)
     } else {
         PortraitHomeScreen(navigateToEncyclopedia)
     }
 }
 
 @Composable
-fun LandscapeHomeScreen(navigateToEncyclopedia: () -> Unit) {
+private fun LandscapeHomeScreen(
+    navigateToEncyclopedia: () -> Unit,
+    ) {
     Box(modifier = Modifier
         .fillMaxSize(),
     ){
@@ -47,46 +52,37 @@ fun LandscapeHomeScreen(navigateToEncyclopedia: () -> Unit) {
         )
         Column(
             modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ){
-            Button(
-                onClick = {},
-                modifier = Modifier
-                    .padding(16.dp)
-                    .width(192.dp)
-                    .height(48.dp),
-                shape = MaterialTheme.shapes.medium
-            ) {
-                Text(text = "Play", style = MaterialTheme.typography.labelLarge)
-            }
-            Button(
-                onClick = {},
-                modifier = Modifier
-                    .padding(16.dp)
-                    .width(192.dp)
-                    .height(48.dp),
-                shape = MaterialTheme.shapes.medium
-
-            ) {
-                Text(text = "My Collection", style = MaterialTheme.typography.labelLarge)
-            }
-            Button(
-                onClick = navigateToEncyclopedia,
-                modifier = Modifier
-                    .padding(16.dp)
-                    .width(192.dp)
-                    .height(48.dp),
-                shape = MaterialTheme.shapes.medium
-            ) {
-                Text(text = "Encyclopedia", style = MaterialTheme.typography.labelLarge)
-            }
+            Image(
+                painter = painterResource(R.mipmap.ic_launcher_foreground),
+                contentDescription = "",
+                modifier = Modifier.size(192.dp)
+            )
+            HomeButton(
+                modifier = Modifier.padding(8.dp),
+                image = R.drawable.button_play,
+                contentDescription = "Play button",
+                onButtonClicked = {}
+            )
+            HomeButton(
+                modifier = Modifier.padding(8.dp),
+                image = R.drawable.button_collection,
+                contentDescription = "Collection button",
+                onButtonClicked = {}
+            )
+            HomeButton(
+                modifier = Modifier.padding(8.dp),
+                image = R.drawable.button_encyclopedia,
+                contentDescription = "Encyclopedia button",
+                onButtonClicked = navigateToEncyclopedia
+            )
         }
     }
 }
 
 @Composable
-fun PortraitHomeScreen(navigateToEncyclopedia: () -> Unit) {
+private fun PortraitHomeScreen(navigateToEncyclopedia: () -> Unit) {
     Box(modifier = Modifier
         .fillMaxSize(),
     ){
@@ -101,42 +97,61 @@ fun PortraitHomeScreen(navigateToEncyclopedia: () -> Unit) {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ){
-            Spacer(modifier = Modifier.fillMaxSize().weight(0.6f))
-            Button(
-                onClick = {},
-                modifier = Modifier
-                    .padding(16.dp)
-                    .width(192.dp)
-                    .height(48.dp)
-                    .weight(0.1f),
-                shape = MaterialTheme.shapes.medium
-            ) {
-                Text(text = "Play", style = MaterialTheme.typography.labelLarge)
-            }
-            Button(
-                onClick = {},
-                modifier = Modifier
-                    .padding(16.dp)
-                    .width(192.dp)
-                    .height(48.dp)
-                    .weight(0.1f),
-                shape = MaterialTheme.shapes.medium
+            Spacer(modifier = Modifier.fillMaxSize().weight(0.2f))
 
-            ) {
-                Text(text = "My Collection", style = MaterialTheme.typography.labelLarge)
-            }
-            Button(
-                onClick = navigateToEncyclopedia,
-                modifier = Modifier
-                    .padding(16.dp)
-                    .width(192.dp)
-                    .height(48.dp)
-                    .weight(0.1f),
-                shape = MaterialTheme.shapes.medium
-            ) {
-                Text(text = "Encyclopedia", style = MaterialTheme.typography.labelLarge)
-            }
+            Image(
+                painter = painterResource(R.mipmap.ic_launcher_foreground),
+                contentDescription = "",
+                modifier = Modifier.fillMaxSize().weight(0.2f),
+            )
+
+            Spacer(modifier = Modifier.fillMaxSize().weight(0.2f))
+
+            HomeButton(
+                modifier = Modifier.weight(0.1f),
+                image = R.drawable.button_play,
+                contentDescription = "Play button",
+                onButtonClicked = {}
+            )
+            HomeButton(
+                modifier = Modifier.weight(0.1f),
+                image = R.drawable.button_collection,
+                contentDescription = "Collection button",
+                onButtonClicked = {}
+            )
+            HomeButton(
+                modifier = Modifier.weight(0.1f),
+                image = R.drawable.button_encyclopedia,
+                contentDescription = "Encyclopedia button",
+                onButtonClicked = navigateToEncyclopedia
+            )
             Spacer(modifier = Modifier.fillMaxSize().weight(0.1f))
         }
+    }
+}
+
+@Composable
+private fun HomeButton(
+    modifier: Modifier = Modifier,
+    @DrawableRes image: Int,
+    contentDescription: String,
+    onButtonClicked: () -> Unit
+) {
+    Button(
+        onClick = onButtonClicked,
+        modifier = modifier
+            .width(192.dp)
+            .height(48.dp),
+        contentPadding = PaddingValues(0.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent,
+            contentColor = Color.Transparent
+        )
+    ) {
+        Image(
+            painter = painterResource(image),
+            contentDescription = contentDescription,
+            modifier = Modifier.fillMaxSize()
+        )
     }
 }
