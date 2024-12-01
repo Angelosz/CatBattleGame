17978ad50c3e43bb475f102e737c3c288456f163
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-12-01 09:43:02 +0000

    Creates PlayerDao

---
+++ app/src/main/java/angelosz/catbattlegame/data/dao/PlayerDao.kt
@@ -0,0 +1,35 @@
+package angelosz.catbattlegame.data.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import angelosz.catbattlegame.domain.models.entities.OwnedCat
+import angelosz.catbattlegame.domain.models.entities.PlayerAccount
+
+@Dao
+interface PlayerDao {
+    /* Player Account */
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertOrUpdateAccount(playerAccount: PlayerAccount)
+
+    @Query("Select * from player_account Limit 1")
+    suspend fun getPlayerAccount(): PlayerAccount?
+
+
+    /* Owned Cats */
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertOwnedCat(ownedCat: OwnedCat)
+    @Update
+    suspend fun updateOwnedCat(ownedCat: OwnedCat)
+    @Delete
+    suspend fun deleteOwnedCat(ownedCat: OwnedCat)
+
+    @Query("Select * from player_owned_cat")
+    suspend fun getAllOwnedCats(): List<OwnedCat>
+    @Query("Select * from player_owned_cat where id = :catId")
+    suspend fun getOwnedCatByCatId(catId: Int): OwnedCat
+
+}
