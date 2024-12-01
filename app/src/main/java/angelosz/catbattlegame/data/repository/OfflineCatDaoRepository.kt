d65a363c1e248be63929afd25146b7360a35fba0
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-12-01 10:37:18 +0000

    Adds getCatsById to get a list of cats from a list of ids

--- app/src/main/java/angelosz/catbattlegame/data/repository/OfflineCatDaoRepository.kt
+++ app/src/main/java/angelosz/catbattlegame/data/repository/OfflineCatDaoRepository.kt
@@ -4,10 +4,8 @@ import angelosz.catbattlegame.data.dao.CatDao
 import angelosz.catbattlegame.domain.models.entities.Cat
 
 class OfflineCatDaoRepository(private val catDao: CatDao): CatRepository {
-
     override suspend fun getAllCats() = catDao.getAllCats()
-
     override suspend fun getCatById(id: Int) = catDao.getCatById(id)
-
-    override suspend fun InsertCats(cats: List<Cat>) = catDao.insertCats(cats)
+    override suspend fun insertCats(cats: List<Cat>) = catDao.insertCats(cats)
+    override suspend fun getCatsById(ids: List<Int>): List<Cat> = catDao.getCatsById(ids)
 }
