579e29ab2bd07ead57cca39c5779a6e0ac961bd6
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-12-01 09:43:17 +0000

    Creates PlayerAccountRepository Interface

---
+++ app/src/main/java/angelosz/catbattlegame/data/repository/PlayerAccountRepository.kt
@@ -0,0 +1,18 @@
+package angelosz.catbattlegame.data.repository
+
+import angelosz.catbattlegame.domain.models.entities.OwnedCat
+import angelosz.catbattlegame.domain.models.entities.PlayerAccount
+
+interface PlayerAccountRepository {
+    /* Player Account */
+    suspend fun getPlayerAccount(): PlayerAccount?
+    suspend fun createOrUpdateAccount(playerAccount: PlayerAccount)
+
+    /* Owned Cats */
+    suspend fun insertOwnedCat(ownedCat: OwnedCat)
+    suspend fun updateOwnedCat(ownedCat: OwnedCat)
+    suspend fun deleteOwnedCat(ownedCat: OwnedCat)
+
+    suspend fun getAllOwnedCats(): List<OwnedCat>
+    suspend fun getOwnedCatByCatId(catId: Int): OwnedCat
+}
