2e093279df03e6f2d2a698d189563bab2552808f
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-12-01 09:44:14 +0000

    Adds playerAccount initialization at the beginning of the app

--- app/src/main/java/angelosz/catbattlegame/MainActivity.kt
+++ app/src/main/java/angelosz/catbattlegame/MainActivity.kt
@@ -6,8 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.lifecycle.lifecycleScope
+import angelosz.catbattlegame.data.database.AppDatabase
+import angelosz.catbattlegame.domain.models.entities.PlayerAccount
 import angelosz.catbattlegame.ui.screens.AppLayout
 import angelosz.catbattlegame.ui.theme.CatBattleGameTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
@@ -16,10 +20,23 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        lifecycleScope.launch {
+            initializePlayerAccount()
+        }
+
         setContent {
             CatBattleGameTheme {
                 AppLayout(windowSize = calculateWindowSizeClass(this).widthSizeClass)
             }
+        }
+    }
+
+    private suspend fun initializePlayerAccount() {
+        val playerDao = AppDatabase.getInstance(applicationContext).playerDao()
+        val existingAccount = playerDao.getPlayerAccount()
+        if(existingAccount == null){
+            playerDao.insertOrUpdateAccount(PlayerAccount())
         }
     }
 
