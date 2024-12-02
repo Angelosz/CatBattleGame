e69eff8cae0bdb890579e85efe6bf9b961476be8
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-12-02 10:45:44 +0000

    Renames OwnedCatData to OwnedCatDetailsData Adds evolutionCat pair to the data class for the evolution ID and Name

--- app/src/main/java/angelosz/catbattlegame/domain/models/OwnedCatDetailsData.kt
+++ app/src/main/java/angelosz/catbattlegame/domain/models/OwnedCatDetailsData.kt
@@ -3,9 +3,10 @@ package angelosz.catbattlegame.domain.models
 import angelosz.catbattlegame.domain.models.entities.Ability
 import angelosz.catbattlegame.domain.models.entities.Cat
 
-data class OwnedCatData(
+data class OwnedCatDetailsData(
     val cat: Cat,
     val abilities: List<Ability>,
     val level: Int,
     val experience: Int,
+    val evolutionCat: Pair<Int, String>?
 )
