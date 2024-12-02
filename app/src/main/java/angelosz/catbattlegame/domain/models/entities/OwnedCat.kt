36930f448798c99e58ad186b3f2af12b4a8e7065
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-12-02 10:43:54 +0000

    Adds health, attack, defense and attackspeed modifiers to the OwnedCat Entity

--- app/src/main/java/angelosz/catbattlegame/domain/models/entities/OwnedCat.kt
+++ app/src/main/java/angelosz/catbattlegame/domain/models/entities/OwnedCat.kt
@@ -8,6 +8,10 @@ data class OwnedCat(
     @PrimaryKey
     val id: Int = 1,
     val catId: Int,
+    val healthModifier: Int = 0,
+    val attackModifier: Int = 0,
+    val defenseModifier: Int = 0,
+    val attackSpeedModifier: Float = 0.0f,
     val level: Int,
     val experience: Int
 )
