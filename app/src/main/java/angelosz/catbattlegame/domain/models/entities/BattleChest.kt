1a06078e93ac8a3878540e753d66b7afa5840d6b
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-12-02 10:52:11 +0000

    Creates BattleChest data class (Room Entity)

---
+++ app/src/main/java/angelosz/catbattlegame/domain/models/entities/BattleChest.kt
@@ -0,0 +1,15 @@
+package angelosz.catbattlegame.domain.models.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import angelosz.catbattlegame.domain.enums.BattleChestType
+import angelosz.catbattlegame.domain.enums.CatRarity
+
+@Entity(tableName = "battle_chests")
+data class BattleChest(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val rarity: CatRarity = CatRarity.KITTEN,
+    val type: BattleChestType = BattleChestType.NORMAL
+)
+
