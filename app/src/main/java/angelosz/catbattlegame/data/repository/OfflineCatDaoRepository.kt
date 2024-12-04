9309e6bf0edc2922eb5719ecc45f8294b0d8609c
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-12-04 09:47:49 +0000

    Adds new functions to the OfflineCatDaoRepository

--- app/src/main/java/angelosz/catbattlegame/data/repository/OfflineCatDaoRepository.kt
+++ app/src/main/java/angelosz/catbattlegame/data/repository/OfflineCatDaoRepository.kt
@@ -1,6 +1,7 @@
 package angelosz.catbattlegame.data.repository
 
 import angelosz.catbattlegame.data.dao.CatDao
+import angelosz.catbattlegame.domain.enums.CatRarity
 import angelosz.catbattlegame.domain.models.entities.Cat
 
 class OfflineCatDaoRepository(private val catDao: CatDao): CatRepository {
@@ -8,4 +9,6 @@ class OfflineCatDaoRepository(private val catDao: CatDao): CatRepository {
     override suspend fun getCatById(id: Int) = catDao.getCatById(id)
     override suspend fun insertCats(cats: List<Cat>) = catDao.insertCats(cats)
     override suspend fun getCatsById(ids: List<Int>): List<Cat> = catDao.getCatsById(ids)
+    override suspend fun getRandomCatByRarity(rarity: CatRarity): Cat = catDao.getRandomCatByRarity(rarity)
+    override suspend fun getUnownedCatsOfRarityIds(rarity: CatRarity): List<Int> = catDao.getUnownedCatsOfRarityIds(rarity)
 }
