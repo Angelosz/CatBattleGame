08ea862dbfe35190df41a824d8caae67a45363a6
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-12-09 15:19:41 +0000

    Creates PlayerTeam data class (Room Entity)

---
+++ app/src/main/java/angelosz/catbattlegame/domain/models/entities/PlayerTeam.kt
@@ -0,0 +1,11 @@
+package angelosz.catbattlegame.domain.models.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "player_team")
+data class PlayerTeam (
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val name: String = "Default Team",
+)
