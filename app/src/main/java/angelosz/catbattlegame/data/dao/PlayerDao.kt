17d5da5db4218f4e3a90f3d60676548b05bc960b
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-12-09 15:25:32 +0000

    Adds new functions to PlayerDao for the OwnedCats

--- app/src/main/java/angelosz/catbattlegame/data/dao/PlayerDao.kt
+++ app/src/main/java/angelosz/catbattlegame/data/dao/PlayerDao.kt
@@ -32,11 +32,18 @@ interface PlayerDao {
     suspend fun getAllOwnedCats(): List<OwnedCat>
     @Query("Select * from player_owned_cat where catId = :catId")
     suspend fun getOwnedCatByCatId(catId: Int): OwnedCat
+    @Query("Select * from player_owned_cat where catId in (:catIds)")
+    suspend fun getOwnedCatsByIds(catIds: List<Int>): List<OwnedCat>
+    @Query("Select * from player_owned_cat order by id asc limit :limit offset :offset")
+    suspend fun getPaginatedOwnedCats(limit: Int, offset: Int): List<OwnedCat>
+    @Query("Select count(catId) from player_owned_cat")
+    suspend fun getCount(): Int
+    @Query("Select exists(select 1 from player_owned_cat where catId = :catId)")
+    suspend fun ownsCat(catId: Int): Boolean
 
     /* Battle Chests */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertBattleChest(battleChest: BattleChest)
-
     @Delete
     suspend fun deleteBattleChest(battleChest: BattleChest)
 
