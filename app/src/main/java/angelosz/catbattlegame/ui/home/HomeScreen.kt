04d88c64a653ee2a160911f6f8807d0debe186aa
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-12-10 11:22:53 +0000

    Adds Gold & Crystal to the screen

--- app/src/main/java/angelosz/catbattlegame/ui/home/HomeScreen.kt
+++ app/src/main/java/angelosz/catbattlegame/ui/home/HomeScreen.kt
@@ -5,16 +5,26 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import angelosz.catbattlegame.CatViewModelProvider
 import angelosz.catbattlegame.R
 import angelosz.catbattlegame.ui.components.BackgroundImage
 
@@ -30,6 +40,9 @@ fun HomeScreen(
 ) {
     val isPortraitView = windowSize != WindowWidthSizeClass.Expanded
     val background = if(isPortraitView) R.drawable.homescreen_portrait else R.drawable.homescreen_landscape
+
+    val viewModel:HomeScreenViewModel = viewModel(factory = CatViewModelProvider.Factory)
+    val uiState by viewModel.uiState.collectAsState()
 
     Box(modifier = Modifier
         .fillMaxSize(),
@@ -64,6 +77,40 @@ fun HomeScreen(
                     navigateToCollection = navigateToCollection,
                     navigateToEncyclopedia = navigateToEncyclopedia,
                     onPlayButtonClick = onPlayButtonClick
+                )
+            }
+        }
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .align(Alignment.TopStart)
+                .systemBarsPadding()
+        ){
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.padding(start = 16.dp)
+            ){
+                Image(
+                    painter = painterResource(R.drawable.goldcoins_128),
+                    contentDescription = "Gold coins",
+                    modifier = Modifier.size(32.dp)
+                )
+                Text(
+                    text = "${uiState.gold}",
+                    style = MaterialTheme.typography.labelLarge,
+                    color = Color.White
+                )
+                Spacer(modifier = Modifier.width(12.dp))
+                Image(
+                    painter = painterResource(R.drawable.crystals_128),
+                    contentDescription = "Crystals",
+                    modifier = Modifier.size(32.dp)
+                )
+                Text(
+                    text = "${uiState.crystals}",
+                    style = MaterialTheme.typography.labelLarge,
+                    color = Color.White
                 )
             }
         }
