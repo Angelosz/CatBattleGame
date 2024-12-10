9a865383ab010b8f29640be0ef3bf75dea326789
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-12-10 11:20:34 +0000

    Adds crystals attribute to PlayerAccount Entity

--- app/src/main/java/angelosz/catbattlegame/domain/models/entities/PlayerAccount.kt
+++ app/src/main/java/angelosz/catbattlegame/domain/models/entities/PlayerAccount.kt
@@ -10,6 +10,7 @@ data class PlayerAccount (
     val accountLevel: Int = 1,
     val accountExperience: Int = 0,
     val gold: Int = 1000,
+    val crystals: Int = 0,
     val lastLogin: Long = System.currentTimeMillis(),
     val lastDailyReward: Long = System.currentTimeMillis()
 )
