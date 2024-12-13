05c0d2dafdebaa006d1ce6fcb06d7e7130c9a806
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-12-13 20:36:04 +0000

    Renames CombatMenuUiState & CombatMenuViewModel to CampaignScreenUiState & CampaignScreenViewModel

---
+++ app/src/main/java/angelosz/catbattlegame/ui/combat/CampaignScreenUiState.kt
@@ -0,0 +1,20 @@
+package angelosz.catbattlegame.ui.combat
+
+import angelosz.catbattlegame.domain.enums.ScreenState
+import angelosz.catbattlegame.domain.models.entities.Campaign
+import angelosz.catbattlegame.domain.models.entities.CampaignChapter
+
+enum class CampaignSelectionStage {
+    SELECTING_CAMPAIGN,
+    SELECTING_CHAPTER,
+    CHAPTER_SELECTED
+}
+
+data class CampaignScreenUiState (
+    val screenState: ScreenState = ScreenState.LOADING,
+    val campaigns: List<Campaign> = listOf(),
+    val selectedCampaign: Campaign = Campaign(0),
+    val campaignChapters: List<CampaignChapter> = listOf(),
+    val selectedCampaignChapter: CampaignChapter = CampaignChapter(),
+    val stage: CampaignSelectionStage = CampaignSelectionStage.SELECTING_CAMPAIGN
+)
--- app/src/main/java/angelosz/catbattlegame/ui/combat/CombatMenuUiState.kt
+++
@@ -1,7 +0,0 @@
-package angelosz.catbattlegame.ui.combat
-
-import angelosz.catbattlegame.domain.enums.ScreenState
-
-data class CombatMenuUiState (
-    val screenState: ScreenState = ScreenState.LOADING,
-)
