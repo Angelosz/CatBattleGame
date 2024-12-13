6ef6f6fa057e0b9537f80f69dba4ea7deac23901
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-12-13 20:34:10 +0000

    Creates Campaign and CampaignChapter data classes (Room Entities)

---
+++ app/src/main/java/angelosz/catbattlegame/domain/models/entities/Campaign.kt
@@ -0,0 +1,17 @@
+package angelosz.catbattlegame.domain.models.entities
+
+import androidx.annotation.DrawableRes
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import angelosz.catbattlegame.R
+import angelosz.catbattlegame.domain.enums.CampaignState
+
+@Entity(tableName = "campaign")
+data class Campaign (
+    @PrimaryKey
+    val id: Long,
+    val state: CampaignState = CampaignState.LOCKED,
+    val name: String = "Campaign",
+    val description: String = "Campaign description",
+    @DrawableRes val image: Int = R.drawable.house_campaign_icon_256
+)
