5f4fb7b172c897dcfe12adfbd47ebf8a71d59c6d
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-12-22 16:24:32 +0000

    Adds DamageType to Abilities

--- app/src/main/java/angelosz/catbattlegame/domain/models/entities/Ability.kt
+++ app/src/main/java/angelosz/catbattlegame/domain/models/entities/Ability.kt
@@ -6,6 +6,7 @@ import androidx.room.PrimaryKey
 import angelosz.catbattlegame.domain.enums.AbilityTarget
 import angelosz.catbattlegame.domain.enums.AbilityType
 import angelosz.catbattlegame.domain.enums.CombatModifiers
+import angelosz.catbattlegame.domain.enums.DamageType
 
 @Entity(tableName = "abilities")
 data class Ability(
@@ -16,6 +17,7 @@ data class Ability(
     val attackSpeedMultiplier: Float, //1 = 100% base attack speed.
     val combatModifier: CombatModifiers?, //CombatModifier, Turn duration
     val abilityType: AbilityType,
+    val damageType: DamageType,
     val targets: AbilityTarget,
     val cooldown: Int,
     @DrawableRes val image: Int,
