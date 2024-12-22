9adc27a8109c36a9a860945adc8c08ae4c78486e
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-12-22 16:25:33 +0000

    Updates AbilityType with new types

--- app/src/main/java/angelosz/catbattlegame/domain/enums/AbilityType.kt
+++ app/src/main/java/angelosz/catbattlegame/domain/enums/AbilityType.kt
@@ -4,12 +4,8 @@ import androidx.annotation.StringRes
 import angelosz.catbattlegame.R
 
 enum class AbilityType(@StringRes val res: Int) {
-    BLUNT(R.string.abilitytype_blunt),
-    PIERCING(R.string.abilitytype_piercing),
-    HEROIC(R.string.abilitytype_heroic),
-    ELEMENTAL(R.string.abilitytype_elemental),
-    ETHEREAL(R.string.abilitytype_ethereal),
     HEALING(R.string.abilitytype_healing),
-    NORMAL(R.string.abilitytype_normal),
+    DAMAGE(R.string.abilitytype_damage),
     STATUS_CHANGING(R.string.abilitytype_status_changing);
 }
+
