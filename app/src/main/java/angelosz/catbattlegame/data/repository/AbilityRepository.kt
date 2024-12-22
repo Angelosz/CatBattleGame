37dccbb478c932fddd127f9aa1bed479168f3619
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-12-22 16:25:02 +0000

    Adds getEnemyCatAbilities function to AbilityDao the repositories

--- app/src/main/java/angelosz/catbattlegame/data/repository/AbilityRepository.kt
+++ app/src/main/java/angelosz/catbattlegame/data/repository/AbilityRepository.kt
@@ -4,10 +4,8 @@ import angelosz.catbattlegame.domain.models.entities.Ability
 
 interface AbilityRepository {
     suspend fun getAllAbilities(): List<Ability>
-
     suspend fun getAbilityById(id: Int): Ability
-
     suspend fun insertAbilities(abilities: List<Ability>)
-
     suspend fun getCatAbilities(catId: Int): List<Ability>
+    suspend fun getEnemyCatAbilities(catId: Int): List<Ability>
 }
