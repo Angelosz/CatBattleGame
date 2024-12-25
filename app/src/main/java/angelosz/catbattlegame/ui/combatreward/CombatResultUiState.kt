3208838e59f004005c3b4ffec81a7e97c23a64bf
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-12-25 02:40:53 +0000

    Adds chapter & team attributes to CombatResultUiState

--- app/src/main/java/angelosz/catbattlegame/ui/combatreward/CombatResultUiState.kt
+++ app/src/main/java/angelosz/catbattlegame/ui/combatreward/CombatResultUiState.kt
@@ -3,9 +3,13 @@ package angelosz.catbattlegame.ui.combatreward
 import angelosz.catbattlegame.domain.enums.CombatResult
 import angelosz.catbattlegame.domain.enums.RewardType
 import angelosz.catbattlegame.domain.enums.ScreenState
+import angelosz.catbattlegame.domain.models.entities.CampaignChapter
 import angelosz.catbattlegame.domain.models.entities.ChapterReward
+import angelosz.catbattlegame.ui.teambuilder.BasicCatData
 
 data class CombatResultUiState(
+    val chapter: CampaignChapter? = null,
+    val team: List<BasicCatData> = emptyList(),
     val chapterReward: List<ChapterReward> = listOf(
         ChapterReward(
             chapterId = 0,
