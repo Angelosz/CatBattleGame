697cf532d53f62bb2c4a59149dd3eb52feb679ed
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-12-25 02:42:58 +0000

    Updates call to AbilityDao function

--- app/src/main/java/angelosz/catbattlegame/data/repository/OfflineAbilityRepository.kt
+++ app/src/main/java/angelosz/catbattlegame/data/repository/OfflineAbilityRepository.kt
@@ -4,7 +4,7 @@ import angelosz.catbattlegame.data.dao.AbilityDao
 import angelosz.catbattlegame.domain.models.entities.Ability
 
 class OfflineAbilityRepository(private val abilityDao: AbilityDao): AbilityRepository {
-    override suspend fun getAllAbilities() = abilityDao.getAllAbilities()
+    override suspend fun getAllPlayerAbilities() = abilityDao.getAllPlayerAbilities()
     override suspend fun getAbilityById(id: Int) = abilityDao.getAbilityById(id)
     override suspend fun insertAbilities(abilities: List<Ability>) = abilityDao.insertAbilities(abilities)
     override suspend fun getCatAbilities(catId: Int) = abilityDao.getCatAbilities(catId)
