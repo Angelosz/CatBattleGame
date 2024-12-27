6196c2325b6726fb78c9646f90f0ff263e78c925
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-12-27 08:56:19 +0000

    Adds default parameters to Ability data class

--- app/src/main/java/angelosz/catbattlegame/data/entities/Ability.kt
+++ app/src/main/java/angelosz/catbattlegame/data/entities/Ability.kt
@@ -3,6 +3,7 @@ package angelosz.catbattlegame.data.entities
 import androidx.annotation.DrawableRes
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import angelosz.catbattlegame.R
 import angelosz.catbattlegame.domain.enums.AbilitySource
 import angelosz.catbattlegame.domain.enums.AbilityTarget
 import angelosz.catbattlegame.domain.enums.AbilityType
@@ -12,17 +13,17 @@ import angelosz.catbattlegame.domain.enums.DamageType
 @Entity(tableName = "abilities")
 data class Ability(
     @PrimaryKey val id: Int,
-    val name: String,
-    val description: String,
-    val damageMultiplier: Float,
-    val attackSpeedMultiplier: Float, //1 = 100% base attack speed.
-    val combatModifier: CombatModifiers?, //CombatModifier, Turn duration
-    val abilityType: AbilityType,
-    val damageType: DamageType,
-    val targets: AbilityTarget,
-    val cooldown: Int,
-    @DrawableRes val image: Int,
-    @DrawableRes val icon: Int,
+    val name: String = "",
+    val description: String = "",
+    val damageMultiplier: Float = 1f,
+    val attackSpeedMultiplier: Float = 1f, //1 = 100% base attack speed.
+    val combatModifier: CombatModifiers? = null, //CombatModifier, Turn duration
+    val abilityType: AbilityType = AbilityType.DAMAGE,
+    val damageType: DamageType = DamageType.PIERCING,
+    val targets: AbilityTarget = AbilityTarget.SINGLE_ENEMY,
+    val cooldown: Int = 0,
+    @DrawableRes val image: Int = R.drawable.ability_quick_attack_256,
+    @DrawableRes val icon: Int = R.drawable.ability_quick_attack_48,
     val abilitySource: AbilitySource = AbilitySource.PLAYER,
 )
 
