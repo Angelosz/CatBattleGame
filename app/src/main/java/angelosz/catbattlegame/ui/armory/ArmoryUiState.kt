02c5e21e3043c847d0b78ef9d70e6c6def657965
Angel Cabrera Gil <angelcabgil@gmail.com>
2025-01-02 10:19:33 +0000

    Updates selectedCollection type to the new CollectionsViews Enumerator

--- app/src/main/java/angelosz/catbattlegame/ui/armory/ArmoryUiState.kt
+++ app/src/main/java/angelosz/catbattlegame/ui/armory/ArmoryUiState.kt
@@ -1,8 +1,9 @@
 package angelosz.catbattlegame.ui.armory
 
 import angelosz.catbattlegame.domain.enums.ScreenState
+import angelosz.catbattlegame.ui.armory.enums.CollectionsView
 
 data class ArmoryUiState (
-    val selectedCollection: ArmoryView = ArmoryView.CATS,
+    val selectedCollection: CollectionsView = CollectionsView.CATS,
     val screenState: ScreenState = ScreenState.INITIALIZING,
 )
