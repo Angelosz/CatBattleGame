05c98c1130af8e72722520c5d97ed128301ca707
Angel Cabrera Gil <angelcabgil@gmail.com>
2025-01-10 20:05:36 +0000

    Adds Poison DamageType

--- app/src/main/java/angelosz/catbattlegame/domain/enums/DamageType.kt
+++ app/src/main/java/angelosz/catbattlegame/domain/enums/DamageType.kt
@@ -1,12 +1,14 @@
 package angelosz.catbattlegame.domain.enums
 
 import androidx.annotation.StringRes
+import androidx.compose.ui.graphics.Color
 import angelosz.catbattlegame.R
 
-enum class DamageType(@StringRes val res: Int) {
-    BLUNT(R.string.abilitytype_blunt),
-    PIERCING(R.string.abilitytype_piercing),
-    HEROIC(R.string.abilitytype_heroic),
-    ELEMENTAL(R.string.abilitytype_elemental),
-    ETHEREAL(R.string.abilitytype_ethereal),
+enum class DamageType(@StringRes val res: Int, val color: Color) {
+    BLUNT(R.string.abilitytype_blunt, Color.Red),
+    PIERCING(R.string.abilitytype_piercing, Color.Red),
+    HEROIC(R.string.abilitytype_heroic, Color.Yellow),
+    ELEMENTAL(R.string.abilitytype_elemental, Color.Magenta),
+    ETHEREAL(R.string.abilitytype_ethereal, Color.Blue),
+    POISON(R.string.poison, Color.Green)
 }
