033008689c3f4391aa4e1af38fe4c5d246f46a0d
Angel Cabrera Gil <angelcabgil@gmail.com>
2025-02-06 01:09:10 +0000

    Makes PaginationButtons arrows more visible

--- app/src/main/java/angelosz/catbattlegame/ui/components/PaginationButtons.kt
+++ app/src/main/java/angelosz/catbattlegame/ui/components/PaginationButtons.kt
@@ -8,6 +8,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -21,23 +24,35 @@ fun PaginationButtons(
         verticalAlignment = Alignment.CenterVertically,
     ) {
         Button(
-            modifier = Modifier.padding(horizontal = 16.dp),
+            modifier = Modifier.padding(horizontal = 14.dp),
             onClick = onPreviousButtonClicked,
             enabled = isNotFirstPage,
         ) {
             Text(
                 text = "<",
-                style = MaterialTheme.typography.labelMedium,
+                style = MaterialTheme.typography.labelLarge.copy(
+                    shadow = Shadow(
+                        color = Color.Black,
+                        offset = Offset(3f, 3f),
+                        blurRadius = 1f
+                    )
+                ),
             )
         }
         Button(
-            modifier = Modifier.padding(horizontal = 16.dp),
+            modifier = Modifier.padding(horizontal = 14.dp),
             onClick = onNextButtonClicked,
             enabled = isNotLastPage
         ) {
             Text(
                 text = ">",
-                style = MaterialTheme.typography.labelMedium,
+                style = MaterialTheme.typography.labelLarge.copy(
+                    shadow = Shadow(
+                        color = Color.Black,
+                        offset = Offset(3f, 3f),
+                        blurRadius = 1f
+                    )
+                ),
             )
         }
     }
